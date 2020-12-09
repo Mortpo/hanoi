c9b73ebea6343bfdf382e874560f8d216dadd5f8
Mortpo <anquetin.r@gmail.com>
2020-12-09 17:14:25 +0100

    Ajout de test

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -11,19 +11,26 @@ public class DisqueTest {
 	Disque dTall = new Disque(3);
 
 	@Test
-	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
-	}
-
-	@Test
+	//Test disque de taille inférieur à disque de taille supérieur
 	public void compareTo_SmallMedium_Negative(){
 		int expected = -1;
 		int actual = dSmall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	//Test disque de taille supérieur à disque de taille inférieur
+	public void compareTo_MediumSmall_Positive(){
+		int expected = 1;
+		int actual = dMedium.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	//Test disque de taille égales
+	public void compareTo_MediumMedium_Zero(){
+		int expected = 0;
+		int actual = dMedium.compareTo(dMedium);
 		assertEquals(expected, actual);
 	}
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -7,8 +7,49 @@ import org.junit.Test;
 public class TourTest {
 
 	@Test
-	public void test() {
+	//Test si la tour est
+	public void empiler_VoidTrue() {
+		boolean expected = true;
+		Tour tower = new Tour(10);
+		Disque d = new Disque(1);
+		boolean actual = tower.empiler(d);
+		assertEquals(expected, actual);
+	}
 
+	@Test
+	//Test si le disque est inférieur au disque existant et que la taille max de la tour n'est pas atteinte
+	public void empiler_NonVide_TailleNonMax_DisqueInferieur_True() {
+		boolean expected = true;
+		Tour tower = new Tour(10);
+		Disque d1 = new Disque(3);
+		Disque d2 = new Disque(2);
+		tower.empiler(d1);
+		boolean actual = tower.empiler(d2);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	//Test si la taille de la tour est max
+	public void empiler_NonVide_TailleMax_False() {
+		boolean expected = false;
+		Tour tower = new Tour(1);
+		Disque d1 = new Disque(3);
+		Disque d2 = new Disque(2);
+		tower.empiler(d1);
+		boolean actual = tower.empiler(d2);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	//Test si le disque ajouter est plus grand que le disque déjà en place
+	public void empiler_NonVide_TailleNonMax_DisqueSuperieur_False() {
+		boolean expected = false;
+		Tour tower = new Tour(10);
+		Disque d1 = new Disque(1);
+		Disque d2 = new Disque(2);
+		tower.empiler(d1);
+		boolean actual = tower.empiler(d2);
+		assertEquals(expected, actual);
 	}
 
 }
